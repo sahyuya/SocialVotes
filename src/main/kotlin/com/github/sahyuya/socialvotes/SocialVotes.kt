c61f40a02d0fe675fb8d621f52dc2e9c2f6d2d28
sahyuya <nananana0852426@gmail.com>
2025-12-10 16:07:36 +0900

    初回コミット

---
+++ src/main/kotlin/com/github/sahyuya/socialvotes/SocialVotes.kt
@@ -0,0 +1,14 @@
+package com.github.sahyuya.socialvotes
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SocialVotes : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
